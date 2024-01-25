4425fd2164b417da8621f430e2a1b6969bf3d7b5
Xiaoxi Li <xiaoxililxx@outlook.com>
2024-01-25 13:19:56 +0100

    some optimiz

--- ConvertSortedListToBST/src/Solution.java
+++ ConvertSortedListToBST/src/Solution.java
@@ -11,18 +11,19 @@ public class Solution {
             nodes.add(head.val);
             head = head.next;
         }
-        int l = 0;
-        int r = nodes.size() - 1;
-        return helper(nodes, l, r);
+        // int l = 0;
+        // int r = nodes.size() - 1;
+        return helper(nodes, 0, nodes.size() - 1);
     }
 
     private TreeNode helper(List<Integer> nodes, int l, int r) {
         if (l > r) {
             return null;
         }
-        int mid = (l + r) / 2;
+
+        // int mid = (l + r) / 2;         // in some cases this may cause oom
+        int mid = l + (r - l) / 2;
         TreeNode root = new TreeNode(nodes.get(mid));
-        
         root.left = helper(nodes, l, mid - 1);
         root.right = helper(nodes, mid + 1, r);
         return root;
