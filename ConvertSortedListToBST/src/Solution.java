8414e43d240dc513d4d55f772e763444070dfd38
Xiaoxi Li <xiaoxililxx@outlook.com>
2024-01-25 13:49:10 +0100

    optimized by using slow & fast pointers

--- ConvertSortedListToBST/src/Solution.java
+++ ConvertSortedListToBST/src/Solution.java
@@ -13,9 +13,17 @@ public class Solution {
         }
         // int l = 0;
         // int r = nodes.size() - 1;
-        return helper(nodes, 0, nodes.size() - 1);
+        //return helper(nodes, 0, nodes.size() - 1);
+        return helper2(head, null);
     }
 
+    /**
+     * this is the naive way by convert ListNode to array, it will cause extra memory consume
+     * @param nodes
+     * @param l
+     * @param r
+     * @return
+     */
     private TreeNode helper(List<Integer> nodes, int l, int r) {
         if (l > r) {
             return null;
@@ -26,6 +34,31 @@ public class Solution {
         TreeNode root = new TreeNode(nodes.get(mid));
         root.left = helper(nodes, l, mid - 1);
         root.right = helper(nodes, mid + 1, r);
+        return root;
+    }
+
+    /**
+     * Approach by using slow & fast pointers, much quicker
+     * @param head
+     * @param tail
+     * @return
+     */
+    private TreeNode helper2(ListNode head, ListNode tail) {
+        ListNode slow = head;
+        ListNode fast = head;
+        if (head == tail) {
+            return null;
+        }
+
+        while (fast != tail && fast.next != tail) {
+            // the slower pointer will just get the middle position of ListNode when faster pointer reach the end of ListNode
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        TreeNode root = new TreeNode(slow.val);
+        root.left = helper2(head, slow);
+        root.right = helper2(slow.next, tail);
         return root;
     }
 }
